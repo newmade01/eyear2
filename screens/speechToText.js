ab53ed8d3f7aa5c6e1cbea6fa037581a44e799e3
newmade01 <newmade01@naver.com>
2020-12-17 02:41:17 +0900

    python

--- screens/speechToText.js
+++ screens/speechToText.js
@@ -12,7 +12,7 @@ class speechToText extends Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
         <Text>음성에서 텍스트 전환</Text>
-
+        <Text>transcribe_streaming_mic.py: 실시간 text출력</Text>
       </View>
 
     );
--- screens/warningSoundToVib.js
+++ screens/warningSoundToVib.js
@@ -12,7 +12,7 @@ class warningSoundToVib extends Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
         <Text>경고음 울리면 진동 발생</Text>
-
+        <Text>sound_recorder.py: 사운드 10초 저장해서 머신러닝으로 사운드 인식</Text> 
       </View>
 
     );
