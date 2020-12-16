b90b667c9a2be3c2110554c08a725c5efaf834be
newmade01 <newmade01@naver.com>
2020-12-17 02:53:20 +0900

    addPython

--- screens/warningSoundToVib.js
+++ screens/warningSoundToVib.js
@@ -13,6 +13,8 @@ class warningSoundToVib extends Component {
 
         <Text>경고음 울리면 진동 발생</Text>
         <Text>sound_recorder.py: 사운드 10초 저장해서 머신러닝으로 사운드 인식</Text> 
+        <Text>일단, 1.소리 인식하면 어떤 종류의 경고음인지 색과 이름 화면에 띄움</Text> 
+        <Text>일단, 2.진동 울리기 기능 추가</Text> 
       </View>
 
     );
